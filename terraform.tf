c32036228adbced2f95f72a5abedf5e5ef4201dc
cvmanasa <160631961+cvmanasa@users.noreply.github.com>
2024-03-04 20:57:31 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -10,7 +10,6 @@ terraform {
 
 # Configure the AWS provider
 provider "aws" {
-  profile = "default"
   region = "ap-south-1"
 }
 # Creating a VPC
@@ -127,10 +126,10 @@ resource "aws_eip" "proj-eip" {
 
 # Creating an ubuntu EC2 instance
 resource "aws_instance" "Prod-Server" {
- ami = "ami-0ef82eeba2c7a0eeb"
+ ami = "ami-07d9b9ddc6cd8dd30"
  instance_type = "t2.micro"
  availability_zone = "ap-south-1b"
- key_name = "chefkeypair"
+ key_name = "projectkey"
  network_interface {
  device_index = 0
  network_interface_id = aws_network_interface.proj-ni.id
